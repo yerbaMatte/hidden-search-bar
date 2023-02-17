5d47c757a5a310f45b4ba2d505c68f1a480ece81
Milosz Lewandowski <milosz.lewandowski@icloud.com>
2023-02-17 23:57:55 +0100

    scroll html

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ btn.addEventListener('click', () => {
   search.classList.toggle('active');
   input.focus();
 });
+
+function builder(x, y) {
+  this.xCap = x;
+  this.yCap = y;
+
+  this.electro = function () {
+    console.log(this.xCap);
+  };
+}
+
+const fo = new builder('abc', 'bom');
+
+const myObj = {
+  first: 'test',
+  fun() {
+    console.log(this.first);
+  },
+};
+
+console.log();
